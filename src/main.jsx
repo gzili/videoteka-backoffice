7e01791fd9ef12c1ecc443ea2c87140869fa1e85
gzili <gustas.zilinskas@gmail.com>
2023-05-20 23:17:35 +0300

    add CssBaseline

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import './main.css';
 import { ApiInit } from "./ApiInit.jsx";
 import { Create } from "./pages/Create.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { CssBaseline } from "@mui/material";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -46,6 +47,7 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <CssBaseline />
     <Auth0Provider
       domain="dev-r9sh2y9g.us.auth0.com"
       clientId="JyuPU3Bo489j177lD0IbysztF27KYnuE"
