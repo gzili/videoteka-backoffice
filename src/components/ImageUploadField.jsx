b6845ab64ce78a0ddf6408f013b4ba9872c90d2d
gzili <gustas.zilinskas@vippsmobilepay.com>
2023-06-01 00:56:23 +0300

    implement image removal in ImageUploadField

--- src/components/ImageUploadField.jsx
+++ src/components/ImageUploadField.jsx
@@ -2,15 +2,49 @@ import { forwardRef } from 'react';
 import { ObjectField } from "./ObjectField.jsx";
 import { ProtectedImage } from "./ProtectedImage.jsx";
 import { Box } from "@mui/material";
+import { Delete } from '@mui/icons-material';
 import { FileUploadInput } from "./FileUploadInput.jsx";
 
 export const ImageUploadField = forwardRef((props, ref) => {
-  const { value, ...inputProps } = props;
+  const { value, onChange, ...inputProps } = props;
+
+  console.log(value);
 
   return (
       <ObjectField label="Thumbnail">
         {value ? (
-            <ProtectedImage sx={{ width: '100%' }} fileId={value} />
+            <Box
+                sx={{
+                  position: 'relative',
+                  borderRadius: '4px',
+                  overflow: 'hidden',
+                  cursor: 'pointer',
+                  '& > .delete-overlay': {
+                    visibility: 'hidden',
+                  },
+                  '&:hover > .delete-overlay': {
+                    visibility: 'visible',
+                  },
+                }}
+                onClick={() => onChange('')}
+            >
+              <Box
+                  className="delete-overlay"
+                  sx={{
+                    position: 'absolute',
+                    inset: 0,
+                    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}
+              >
+                <Box>
+                  <Delete sx={{ color: 'white', fontSize: 40 }} />
+                </Box>
+              </Box>
+              <ProtectedImage sx={{ width: '100%' }} fileId={value} />
+            </Box>
         ) : (
             <Box
                 sx={{
@@ -20,7 +54,7 @@ export const ImageUploadField = forwardRef((props, ref) => {
                   aspectRatio: '16 / 9',
                 }}
             >
-              <FileUploadInput ref={ref} value={value} {...inputProps} />
+              <FileUploadInput ref={ref} value={value} onChange={onChange} {...inputProps} />
             </Box>
         )}
       </ObjectField>
